5863aaed05625b53506807c89c5acf071ecfdff9
Leandro Castillo Borja <leccbo1995@gmail.com>
2023-05-17 03:22:36 -0500

    Mostrar ventana de wellcomeActivity y poner temporizador

--- app/src/main/java/com/leandro1995/healthypet/SplashActivity.kt
+++ app/src/main/java/com/leandro1995/healthypet/SplashActivity.kt
@@ -1,9 +1,13 @@
 package com.leandro1995.healthypet
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.leandro1995.healthypet.activity.WelcomeActivity
+import com.leandro1995.healthypet.config.Setting
+import java.util.concurrent.TimeUnit
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
@@ -11,6 +15,10 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        Thread.sleep(TimeUnit.SECONDS.toMillis(Setting.SPLASH_TIME_OUT))
+
         installSplashScreen().setKeepOnScreenCondition { true }
+
+        startActivity(Intent(this, WelcomeActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/leandro1995/healthypet/config/Setting.kt
@@ -0,0 +1,9 @@
+package com.leandro1995.healthypet.config
+
+class Setting {
+
+    companion object {
+
+        const val SPLASH_TIME_OUT = 2L
+    }
+}
