c0b217051283c05aa89e2e75fbc4d304e99a5978
Leandro Castillo Borja <leccbo1995@gmail.com>
2023-05-17 03:04:15 -0500

    Colocar codigo de ejecucion de splashScreen

--- app/src/main/java/com/leandro1995/healthypet/SplashActivity.kt
+++ app/src/main/java/com/leandro1995/healthypet/SplashActivity.kt
@@ -3,12 +3,14 @@ package com.leandro1995.healthypet
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        installSplashScreen().setKeepOnScreenCondition { true }
     }
 }
