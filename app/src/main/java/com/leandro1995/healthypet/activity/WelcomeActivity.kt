ffa444d3f3d8d96888fc670a957e55146b692f27
Leandro Castillo Borja <leccbo1995@gmail.com>
2023-05-17 20:25:31 -0500

    Colocar método de viewmodel en wellcome

--- app/src/main/java/com/leandro1995/healthypet/activity/WelcomeActivity.kt
+++ app/src/main/java/com/leandro1995/healthypet/activity/WelcomeActivity.kt
@@ -2,16 +2,21 @@ package com.leandro1995.healthypet.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import com.leandro1995.healthypet.R
 import com.leandro1995.healthypet.databinding.ActivityWelcomeBinding
+import com.leandro1995.healthypet.viewmodel.WelcomeViewModel
 
 class WelcomeActivity : AppCompatActivity() {
 
     private lateinit var welcomeBinding: ActivityWelcomeBinding
+
+    private val welcomeViewModel by viewModels<WelcomeViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         welcomeBinding = DataBindingUtil.setContentView(this, R.layout.activity_welcome)
+        welcomeBinding.welcomeViewModel = welcomeViewModel
     }
 }
