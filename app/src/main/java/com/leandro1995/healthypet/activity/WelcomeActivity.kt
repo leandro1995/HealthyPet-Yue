68942a83b3dd9256f810fbaa46dca7b925c52d04
Leandro Castillo Borja <leccbo1995@gmail.com>
2023-05-17 20:16:44 -0500

    Colocar dataBinding en wellcomw

--- app/src/main/java/com/leandro1995/healthypet/activity/WelcomeActivity.kt
+++ app/src/main/java/com/leandro1995/healthypet/activity/WelcomeActivity.kt
@@ -2,11 +2,16 @@ package com.leandro1995.healthypet.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.leandro1995.healthypet.R
+import com.leandro1995.healthypet.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+
+    private lateinit var welcomeBinding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+
+        welcomeBinding = DataBindingUtil.setContentView(this, R.layout.activity_welcome)
     }
 }
---
+++ app/src/main/java/com/leandro1995/healthypet/viewmodel/WelcomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.leandro1995.healthypet.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class WelcomeViewModel : ViewModel() {
+
+}
