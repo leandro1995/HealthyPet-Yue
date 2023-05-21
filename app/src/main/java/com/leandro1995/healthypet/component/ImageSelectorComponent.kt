2ddefdaff51c638e80c8fdf13ede1a00541d8282
Leandro Castillo Borja <leccbo1995@gmail.com>
2023-05-21 12:29:41 -0500

    Mostrar imagen en componente
    Se modifico la version de la libreria por problemas con fresco

--- app/src/main/java/com/leandro1995/healthypet/component/ImageSelectorComponent.kt
+++ app/src/main/java/com/leandro1995/healthypet/component/ImageSelectorComponent.kt
@@ -2,18 +2,31 @@ package com.leandro1995.healthypet.component
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.leandro1995.healthypet.R
 import com.leandro1995.healthypet.activity.CameraActivity
 import com.leandro1995.healthypet.component.ambient.ViewAmbient
+import com.leandro1995.healthypet.config.Setting
 import com.leandro1995.healthypet.databinding.ComponentImageSelectorBinding
+import com.leandro1995.healthypet.extension.putString
+import com.leandro1995.healthypet.model.design.ActivityUtil
+import java.io.File
 
 class ImageSelectorComponent(context: Context, attrs: AttributeSet? = null) :
     ViewAmbient(context, attrs) {
 
     private lateinit var componentImageSelectorBinding: ComponentImageSelectorBinding
+
+    private val resultLauncher = ActivityUtil.activityResultLauncher(
+        activity = (context as AppCompatActivity),
+        resultData = { data ->
+
+            componentImageSelectorBinding.photoProfileSimple.setImageURI(Uri.fromFile(File((data putString Setting.IMAGE_PUT)!!)))
+        })
 
     override fun view() {
 
@@ -37,6 +50,6 @@ class ImageSelectorComponent(context: Context, attrs: AttributeSet? = null) :
 
     private fun starActivityCamera() {
 
-        context.startActivity(Intent(context, CameraActivity::class.java))
+        resultLauncher.launch(Intent(context, CameraActivity::class.java))
     }
 }
