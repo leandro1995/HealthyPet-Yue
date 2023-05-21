fcfab634d6b88bd9e18edb972e7b695a018fc2cc
Leandro Castillo Borja <leccbo1995@gmail.com>
2023-05-21 03:25:16 -0500

    Tomar foto y enviarlo a CropImageActivity

--- app/src/main/java/com/leandro1995/healthypet/activity/CameraActivity.kt
+++ app/src/main/java/com/leandro1995/healthypet/activity/CameraActivity.kt
@@ -1,10 +1,12 @@
 package com.leandro1995.healthypet.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.leandro1995.healthypet.R
+import com.leandro1995.healthypet.config.Setting
 import com.leandro1995.healthypet.config.callback.camera.CameraPhotoCallBack
 import com.leandro1995.healthypet.config.callback.intent.CameraIntentCallBack
 import com.leandro1995.healthypet.config.listener.CameraPhotoListener
@@ -12,6 +14,7 @@ import com.leandro1995.healthypet.databinding.ActivityCameraBinding
 import com.leandro1995.healthypet.extension.lifecycleScope
 import com.leandro1995.healthypet.intent.config.CameraIntentConfig
 import com.leandro1995.healthypet.util.DesignUtil
+import com.leandro1995.healthypet.util.FileUtil
 import com.leandro1995.healthypet.viewmodel.CameraViewModel
 
 class CameraActivity : AppCompatActivity(), CameraIntentCallBack {
@@ -64,7 +67,17 @@ class CameraActivity : AppCompatActivity(), CameraIntentCallBack {
                     cameraPhotoCallBack = object : CameraPhotoCallBack {
 
                         override fun photoByteArray(byteArray: ByteArray) {
-                            
+
+                            startActivity(Intent(
+                                this@CameraActivity, CropImageActivity::class.java
+                            ).apply {
+
+                                putExtra(
+                                    Setting.IMAGE_PUT, FileUtil.photoUrl(
+                                        activity = this@CameraActivity, byteArray = byteArray
+                                    )
+                                )
+                            })
                         }
                     }
                 })
