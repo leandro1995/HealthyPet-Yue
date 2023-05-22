8f7bc34ba3e351cfccdfbce211ec4f2b741651e4
Leandro Castillo Borja <leccbo1995@gmail.com>
2023-05-21 20:21:18 -0500

    Crear modelo para el registro de mascota

--- app/src/main/java/com/leandro1995/healthypet/model/entity/Pet.kt
+++ app/src/main/java/com/leandro1995/healthypet/model/entity/Pet.kt
@@ -1,3 +1,9 @@
 package com.leandro1995.healthypet.model.entity
 
-class Pet
+class Pet constructor(
+    private var photoUrl: String = "",
+    private var name: String = "",
+    private var spice: Spice = Spice(),
+    private var isSex: Boolean = false,
+    private var date: Long = -1L
+)
---
+++ app/src/main/java/com/leandro1995/healthypet/model/entity/Spice.kt
@@ -0,0 +1,3 @@
+package com.leandro1995.healthypet.model.entity
+
+class Spice constructor(private var id: Int = -1, private var name: String = "")
