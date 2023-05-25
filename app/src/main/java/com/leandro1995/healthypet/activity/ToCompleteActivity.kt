ef3c587054caf8b3e183c6c0ba74a513b6d814a6
Leandro Castillo Borja <leccbo1995@gmail.com>
2023-05-24 20:05:27 -0500

    Implemnetar dataBinding en ToCompleteActivity

--- app/src/main/java/com/leandro1995/healthypet/activity/ToCompleteActivity.kt
+++ app/src/main/java/com/leandro1995/healthypet/activity/ToCompleteActivity.kt
@@ -2,14 +2,19 @@ package com.leandro1995.healthypet.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.leandro1995.healthypet.R
+import com.leandro1995.healthypet.databinding.ActivityToCompleteBinding
 import com.leandro1995.healthypet.util.DesignUtil
 
 class ToCompleteActivity : AppCompatActivity() {
 
+    private lateinit var toCompleteBinding: ActivityToCompleteBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_to_complete)
+
+        toCompleteBinding = DataBindingUtil.setContentView(this, R.layout.activity_to_complete)
 
         statusBar()
     }
