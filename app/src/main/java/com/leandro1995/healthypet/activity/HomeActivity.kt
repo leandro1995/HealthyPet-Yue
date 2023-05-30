44d14b3489bf9c7a0f0227faafc0466629be3a31
Leandro Castillo Borja <leccbo1995@gmail.com>
2023-05-30 00:23:37 -0500

    Mostrar opciones del menu en BottomNavigationComponent

--- app/src/main/java/com/leandro1995/healthypet/activity/HomeActivity.kt
+++ app/src/main/java/com/leandro1995/healthypet/activity/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.leandro1995.healthypet.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.leandro1995.healthypet.R
 import com.leandro1995.healthypet.databinding.ActivityHomeBinding
 
@@ -14,5 +15,12 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         homeBinding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        navigationComponent()
+    }
+
+    private fun navigationComponent() {
+
+        homeBinding.bottomNavigationComponent.navigationItem(navController = findNavController(R.id.nav_host_fragment_activity_main))
     }
 }
--- app/src/main/java/com/leandro1995/healthypet/component/BottomNavigationComponent.kt
+++ app/src/main/java/com/leandro1995/healthypet/component/BottomNavigationComponent.kt
@@ -1,17 +1,30 @@
 package com.leandro1995.healthypet.component
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.ui.setupWithNavController
 import com.leandro1995.healthypet.R
 import com.leandro1995.healthypet.component.ambient.ViewAmbient
+import com.leandro1995.healthypet.component.util.TypeArrayComponentUtil
 import com.leandro1995.healthypet.databinding.ComponentBottomNavigationBinding
 
 class BottomNavigationComponent(context: Context, attrs: AttributeSet? = null) :
     ViewAmbient(context, attrs) {
 
     private lateinit var componentBottomNavigationBinding: ComponentBottomNavigationBinding
+
+    init {
+
+        typedArray(
+            typedArray = attributes(
+                intArray = R.styleable.BottomNavigationComponent, attrs = attrs
+            )
+        )
+    }
 
     override fun view() {
 
@@ -21,5 +34,24 @@ class BottomNavigationComponent(context: Context, attrs: AttributeSet? = null) :
             this,
             true
         )
+    }
+
+    fun navigationItem(navController: NavController) {
+
+        componentBottomNavigationBinding.homeNavigation.setupWithNavController(navController)
+    }
+
+    override fun typedArray(typedArray: TypedArray?) {
+
+        componentBottomNavigationBinding.homeNavigation.apply {
+
+            menu.clear()
+            inflateMenu(
+                TypeArrayComponentUtil.typeArrayMenu(
+                    typedArray = typedArray,
+                    indexAttr = R.styleable.BottomNavigationComponent_menu_bottom_navigation
+                )
+            )
+        }
     }
 }
