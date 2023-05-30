3f9c5a6fb59ddb8782f671d3d099dc55c741305e
Leandro Castillo Borja <leccbo1995@gmail.com>
2023-05-29 23:52:34 -0500

    Implementar databinding en homeActivity

--- app/src/main/java/com/leandro1995/healthypet/activity/HomeActivity.kt
+++ app/src/main/java/com/leandro1995/healthypet/activity/HomeActivity.kt
@@ -2,12 +2,17 @@ package com.leandro1995.healthypet.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.leandro1995.healthypet.R
+import com.leandro1995.healthypet.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
+    private lateinit var homeBinding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        homeBinding = DataBindingUtil.setContentView(this, R.layout.activity_home)
     }
 }
