2cfd03bec3e63831925e67f64b63150f7c70c0ea
Leandro Castillo Borja <leccbo1995@gmail.com>
2023-06-03 17:24:37 -0500

    Implementar binding en RegisterInjectionActivity

--- app/src/main/java/com/leandro1995/healthypet/activity/RegisterInjectionActivity.kt
+++ app/src/main/java/com/leandro1995/healthypet/activity/RegisterInjectionActivity.kt
@@ -2,12 +2,18 @@ package com.leandro1995.healthypet.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.leandro1995.healthypet.R
+import com.leandro1995.healthypet.databinding.ActivityRegisterInjectionBinding
 
 class RegisterInjectionActivity : AppCompatActivity() {
-    
+
+    private lateinit var registerInjectionBinding: ActivityRegisterInjectionBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register_injection)
+
+        registerInjectionBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_register_injection)
     }
 }
