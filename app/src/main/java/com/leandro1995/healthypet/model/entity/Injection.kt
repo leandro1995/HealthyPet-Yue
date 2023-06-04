b48976ccb4acdde4ea056e47d6e3ec662a4f808c
Leandro Castillo Borja <leccbo1995@gmail.com>
2023-06-04 13:23:25 -0500

    Agregar campo url en Injection

--- app/src/main/java/com/leandro1995/healthypet/model/entity/Injection.kt
+++ app/src/main/java/com/leandro1995/healthypet/model/entity/Injection.kt
@@ -2,6 +2,7 @@ package com.leandro1995.healthypet.model.entity
 
 class Injection constructor(
     private val id: Int = -1,
+    private val photoUrl: String = "",
     private val currentDate: Long = -1L,
     private val nextAppointment: Long = -1L,
     private val typeInjection: TypeInjection = TypeInjection(),
