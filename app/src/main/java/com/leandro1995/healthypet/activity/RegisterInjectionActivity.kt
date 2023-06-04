bc28a9b596ecb29326b493f85873e8f03a3c5e11
Leandro Castillo Borja <leccbo1995@gmail.com>
2023-06-03 19:05:26 -0500

    Colocar materialToolbar en RegisterInjectionActivity

--- app/src/main/java/com/leandro1995/healthypet/activity/RegisterInjectionActivity.kt
+++ app/src/main/java/com/leandro1995/healthypet/activity/RegisterInjectionActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.leandro1995.healthypet.R
 import com.leandro1995.healthypet.databinding.ActivityRegisterInjectionBinding
+import com.leandro1995.healthypet.util.DesignUtil
 
 class RegisterInjectionActivity : AppCompatActivity() {
 
@@ -15,5 +16,16 @@ class RegisterInjectionActivity : AppCompatActivity() {
 
         registerInjectionBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_register_injection)
+
+        materialToolbar()
+    }
+
+    private fun materialToolbar() {
+
+        DesignUtil.materialToolbar(activity = this,
+            materialToolbar = registerInjectionBinding.appBar.toolbar,
+            idTitle = R.string.register_injection_text_title,
+            isArrow = true,
+            method = { finish() })
     }
 }
