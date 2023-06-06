cc992f2efeab2d13e2c2199234ac0c23140be4a3
Leandro Castillo Borja <leccbo1995@gmail.com>
2023-06-05 23:06:32 -0500

    Mostrar lista o mensaje de error cuando hay datos en InjectionListComponent

--- app/src/main/java/com/leandro1995/healthypet/component/InjectionListComponent.kt
+++ app/src/main/java/com/leandro1995/healthypet/component/InjectionListComponent.kt
@@ -8,7 +8,19 @@ import com.leandro1995.healthypet.model.entity.Injection
 class InjectionListComponent(context: Context, attrs: AttributeSet? = null) :
     ListViewAmbient(context, attrs) {
 
+    private val injectionArrayList = arrayListOf<Injection>()
+
     fun injectionArrayList(injectionArrayList: ArrayList<Injection>) {
 
+        this.injectionArrayList.clear()
+        this.injectionArrayList.addAll(injectionArrayList)
+
+        if (this.injectionArrayList.isEmpty()) {
+
+            errorMessageVisible()
+        } else {
+
+            errorMessageGone()
+        }
     }
 }
